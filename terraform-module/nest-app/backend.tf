5ca48b66edb22257e02b15c3b505005dea34cf54
Segun Ofe <ofesegunayodeji@gmail.com>
2026-03-27 00:19:27 -0700

    replaced statelock with uselockfile in terraform backend file

--- terraform-module/nest-app/backend.tf
+++ terraform-module/nest-app/backend.tf
@@ -4,8 +4,8 @@ terraform {
     key     = "terraform-module/nest/ecs/terraform.tfstate" #the key lets me use the same bucket for different environments and resources
     region  = "us-east-2"
     encrypt = true
-
-    dynamodb_table = "terraform-state-lock" # the name of the dynamoDB table 
-    profile        = "default"              # profile is the name of the AWS CLI profile to use for authentication
+    use_lockfile   = true    # replaces dynamodb_table
+    # dynamodb_table = "terraform-state-lock" # the name of the dynamoDB table 
+    # profile        = "default"              # profile is the name of the AWS CLI profile to use for authentication
   }
 }
